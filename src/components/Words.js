6040d410c9584f82c776dcf52f8c97c3089efd30
AdamJablonka <50035910+AdamJablonka@users.noreply.github.com>
2022-08-12 18:17:55 -0400

    Update Words.js

--- src/components/Words.js
+++ src/components/Words.js
@@ -4,7 +4,7 @@ const Words = ( {wordArray} ) => {
     return(
         <div>
             <h2>
-            {wordArray[0]} {wordArray[1]} {wordArray[2]} {wordArray[3]} {wordArray[5]} {wordArray[6]} {wordArray[7]} {wordArray[8]} {wordArray[9]} {wordArray[10]} {wordArray[11]} {wordArray[12]} {wordArray[13]} {wordArray[14]} {wordArray[15]} {wordArray[16]}
+            {wordArray[0]} {wordArray[1]} {wordArray[2]} {wordArray[3]} {wordArray[4]} {wordArray[5]} {wordArray[6]} {wordArray[7]} {wordArray[8]} {wordArray[9]} {wordArray[10]} {wordArray[11]} {wordArray[12]} {wordArray[13]} {wordArray[14]} {wordArray[15]} {wordArray[16]}
             </h2>
         </div>
     )
