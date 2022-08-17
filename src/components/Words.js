08919dba78be1e78044ae30944b6be975365a985
AdamJablonka <50035910+AdamJablonka@users.noreply.github.com>
2022-08-16 21:20:20 -0400

    Update Words.js

--- src/components/Words.js
+++ src/components/Words.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Words = ( {wordArray} ) => {
     return(
         <div>
-            <h2>
+            <p>
             {wordArray[0]} {wordArray[1]} {wordArray[2]} {wordArray[3]} {wordArray[4]} {wordArray[5]} {wordArray[6]} {wordArray[7]} {wordArray[8]} {wordArray[9]} {wordArray[10]} {wordArray[11]} {wordArray[12]} {wordArray[13]} {wordArray[14]} {wordArray[15]} {wordArray[16]}
-            </h2>
+            </p>
         </div>
     )
 }
